daa777ba0df8e6e67c95d47ea888aec2c4e60f4a
fernanda <ferchanovas@hotmail.com>
2016-07-08 11:04:58 -0500

    fer

---
+++ hola.py
@@ -0,0 +1,5 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:18:55) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print ("hola esta es mi primera vez en paython")
+hola esta es mi primera vez en paython
+>>> 
